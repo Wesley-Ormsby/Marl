3462aac85e8e38e2f8d1403d67b013457ac2f49a
Wesley Ormsby <wesley_ormsby@outlook.com>
2023-07-26 10:00:28 -0400

    Fixed CLI

--- src/cli.js
+++ src/cli.js
@@ -7,12 +7,13 @@ const [, , filepath] = process.argv;
 
 const absPath = resolve(process.cwd(), filepath);
 if(filepath.split(".")[1] != "marl" && filepath.split(".")[1] != "txt") {
-  console.log("x1b[1m\x1b[31mError: Invalid file extension. File must be '.marl' or '.txt'")
-}
-fs.readFile(absPath, "utf8", function (err, data) {
+  console.log("\x1b[1m\x1b[31mError: Invalid file extension. File must be '.marl' or '.txt'")
+} else {
+  fs.readFile(absPath, "utf8", function (err, data) {
     if (err) {
       console.log(`x1b[1m\x1b[31mError: File '${filepath}' does not exist`)
     } else {
       marl(data);
     }
   });
+}
